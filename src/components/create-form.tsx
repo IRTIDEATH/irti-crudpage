9033024a1eca2dcf7567b39ef74147c3bf81d9d4
IRTIDEATH <enderjhass@gmail.com>
2024-06-18 11:19:07 +0700

    upd: create data working correctly

--- src/components/create-form.tsx
+++ src/components/create-form.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { saveContact } from "@/lib/actions";
+import { useFormState } from "react-dom";
 
 const CreateForm = () => {
 
+  const [state, formAction] = useFormState(saveContact, null)
+
   return (
     <div>
-      <form action={saveContact}>
+      <form action={formAction}>
         <div className="mb-5">
           <label
             htmlFor="name"
@@ -19,6 +24,9 @@ const CreateForm = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Full Name..."
           />
+          <div id="name-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
+          </div>
         </div>
         <div className="mb-5">
           <label
@@ -34,6 +42,12 @@ const CreateForm = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Phone Number..."
           />
+          <div id="phone-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
+          </div>
+        </div>
+        <div id="message-error" aria-live="polite" aria-atomic="true">
+          <p className="mt-2 text-sm text-red-500">{state?.message}</p>
         </div>
 
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center">save</button>
